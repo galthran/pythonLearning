b2b44914b1dad460d507dc7670a00e496aa9729e
galthran80 <rafal.jarosinski@gmail.com>
2022-10-25 21:50:13 +0200

    Learning the basics

---
+++ Input.py
@@ -0,0 +1,4 @@
+name = input("What is you name ")
+age = input("How old are you ")
+
+print(name + " is " + age + " years old")
---
+++ Math.py
@@ -0,0 +1,24 @@
+from math import *
+
+age = 16
+print("I'm " + str(age) + " years old")
+
+print(max(1, 3, 5, 7, 9, 22, 8))
+
+x = 5
+y = 6
+z = x + y
+
+print(z)
+
+print(pow(5, 2))
+
+print(round(7 / 3))
+
+print(sqrt(25))
+
+print(ceil(5.00001))
+
+print(floor(5.511))
+
+i = 5
