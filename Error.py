777d17f4791ab53da4bf687551d4613195d529dd
galthran80 <rafal.jarosinski@gmail.com>
2022-10-31 18:32:41 +0100

    Dealing with errors

---
+++ Error.py
@@ -0,0 +1,17 @@
+
+error = True
+
+a = input("Put a number: ")
+print(int(a) + 1)
+
+# while error:
+#     try:
+#         a = input("Put a number: ")
+#         print(int(a) + 1)
+#         error = False
+#     except ValueError:
+#         print("Put a correct number")
+#         error = True
+
+
+
