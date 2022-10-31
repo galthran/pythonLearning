68180540c72e0e538208ad4ba933ffa35dce9b18
galthran80 <rafal.jarosinski@gmail.com>
2022-10-31 16:20:11 +0100

    Learning about loops

---
+++ ForLoop.py
@@ -0,0 +1,13 @@
+colors = ["red", "blue", "green", "white"]
+
+for color in colors:
+    print(color)
+
+for letter in "white":
+    print(letter)
+
+for i in range(6):
+    print(i)
+
+for i in range(10, 20):
+    print(i)
---
+++ WhileLoop.py
@@ -0,0 +1,8 @@
+i = 25
+k = 2000
+
+while i < k:
+    print("i: " + str(i))
+    i = i + 5
+
+print("Current i value: " + str(i))
